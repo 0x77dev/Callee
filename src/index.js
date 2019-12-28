013cd4c8ba516c8e1ff1ba010730ab14ac57a51e
Misha Marinenko <0x77dev@protonmail.com>
2019-12-28 10:03:13 +0200

    Base concept

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import Peer from "peerjs";
 
+let myMediaStream;
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+if (navigator.getUserMedia) {
+  navigator.getUserMedia({ audio: true, video: { width: 800, height: 600 } },
+    (stream) => {
+      myMediaStream = stream;
+    },
+    (err) => {
+      document.writeln("The following error occurred: " + err.name);
+    }
+  );
+} else {
+  document.writeln("getUserMedia not supported");
+}
+
+
 let peer = new Peer();
 
 document.writeln(`
@@ -8,8 +26,32 @@ document.writeln(`
   </pre>
 `);
 
+
+let video = document.createElement('video');
+
 peer.on("open", id => {
   document.writeln("ID:", id, "<br>");
-  const callTo = prompt('Enter id to call to (answer "n" to receive)', "n");
-  if (callTo !== "n") document.writeln("Calling to", callTo, "<br>");
+  StartCall();
 });
+
+peer.on('call', (call) => {
+  document.writeln("<b>Call Incoming!!!</b>", call.toString());
+  call.answer(myMediaStream);
+  call.on('stream', (stream) => {
+    // `stream` is the MediaStream of the remote peer.
+    // Here you'd add it to an HTML video/canvas element.
+    video.srcObject = stream;
+    video.onloadedmetadata =  (e) => {
+      video.play();
+    };
+  });
+});
+
+let StartCall = () => {
+  const callTo = prompt('Enter id to call to (answer "n" to receive)', "n");
+  if (callTo !== "n") {
+    document.writeln("Calling to", callTo, "<br>");
+    let call = peer.call(callTo, myMediaStream);
+    document.writeln("Call", call.toString(), "<br>");
+  }
+}
